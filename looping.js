ea88611d71433f1db06b64a9b69d6cfc01993902
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-27 03:41:45 +0600

    Looping JS File Done

---
+++ looping.js
@@ -0,0 +1,14 @@
+const numbers = [ 5, 6, 8, 12, 24, 16];
+for (let i = 0; i < numbers.length; i++) {
+
+}
+
+for (const num of numbers) {
+    // console.log(num);
+}
+
+const player = "Ab De Villiars";
+for (const char of player) {
+    console.log(char);
+}
+
